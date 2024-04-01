dfabe046dfc41f6d9015a0ee63a406f77e3af479
박병진 <pijoh3@naver.com>
2024-04-01 22:33:41 +0900

    공통 로직 리팩토링

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,29 +1,10 @@
-'use client'
+"use client";
 
-import Image from "next/image";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
-import styles from "@/app/page.module.css";
-import zLogo from "../../../../public/zlogo.png";
+import Main from "@/app/(beforeLogin)/_component/Main";
 
 export default function Login() {
-  const router = useRouter()
+  const router = useRouter();
   router.replace("/i/flow/login");
-  return (
-    <>
-      <div className={styles.left}>
-        <Image src={zLogo} alt="logo" />
-      </div>
-      <div className={styles.right}>
-        <h1>지금 일어나고 있는 일</h1>
-        <Link href="i/flow/signup" className={styles.signup}>
-          계정 만들기
-        </Link>
-        <h3>이미 트위터에 가입하셨나요?</h3>
-        <Link href="/login" className={styles.login}>
-          로그인
-        </Link>
-      </div>
-    </>
-  );
+  return <Main />;
 }
