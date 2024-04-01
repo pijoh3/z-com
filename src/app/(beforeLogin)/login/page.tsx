188e49be61cd16e63d3e2f5c6224107523a58563
박병진 <pijoh3@naver.com>
2024-04-01 22:26:58 +0900

    로그인 모달 내용 추가

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,5 +1,29 @@
-import { redirect } from "next/navigation";
+'use client'
+
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import styles from "@/app/page.module.css";
+import zLogo from "../../../../public/zlogo.png";
 
 export default function Login() {
-  redirect("/i/flow/login");
+  const router = useRouter()
+  router.replace("/i/flow/login");
+  return (
+    <>
+      <div className={styles.left}>
+        <Image src={zLogo} alt="logo" />
+      </div>
+      <div className={styles.right}>
+        <h1>지금 일어나고 있는 일</h1>
+        <Link href="i/flow/signup" className={styles.signup}>
+          계정 만들기
+        </Link>
+        <h3>이미 트위터에 가입하셨나요?</h3>
+        <Link href="/login" className={styles.login}>
+          로그인
+        </Link>
+      </div>
+    </>
+  );
 }
--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <h1>지금 일어나고 있는 일</h1>
         <Link href="i/flow/signup" className={styles.signup}>계정 만들기</Link>
         <h3>이미 트위터에 가입하셨나요?</h3>
-        <Link href="i/flow/login" className={styles.login}>로그인</Link>
+        <Link href="/login" className={styles.login}>로그인</Link>
       </div>
     </>
   );
