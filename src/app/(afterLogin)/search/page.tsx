592b6a063d2db705e56e194ad8df83af0d7cb3d0
Park Byeoung Jin <pijoh3@naver.com>
2024-05-13 09:52:03 +0900

    검색하기 추가

--- src/app/(afterLogin)/search/page.tsx
+++ src/app/(afterLogin)/search/page.tsx
@@ -1,3 +1,41 @@
-export default function Home() {
-  return <main>쪽지페이지</main>;
+import BackButton from "@/app/(afterLogin)/_component/BackButton";
+import SearchForm from "@/app/(afterLogin)/_component/SearchForm";
+import Post from "@/app/(afterLogin)/_component/Post";
+import Tab from "./_component/Tab";
+import style from "./search.module.css";
+
+type Props = {
+  searchParams: { q: string; f?: string; pf?: string };
+};
+
+export default function Search({ searchParams }: Props) {
+  return (
+    <main className={style.main}>
+      <div className={style.searchTop}>
+        <div className={style.searchZone}>
+          <div className={style.buttonZone}>
+            <BackButton />
+          </div>
+          <div className={style.formZone}>
+            <SearchForm q={searchParams.q} />
+          </div>
+        </div>
+        <Tab />
+      </div>
+      <div className={style.list}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        {/*<SearchResult searchParams={searchParams} />*/}
+      </div>
+    </main>
+  );
 }
