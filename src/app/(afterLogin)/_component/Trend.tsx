b9911a2cf1b36912bbad4beb926de83c6bf00ff9
Park Byeoung Jin <pijoh3@naver.com>
2024-06-04 21:53:47 +0900

    url 인코딩 추가

--- src/app/(afterLogin)/_component/Trend.tsx
+++ src/app/(afterLogin)/_component/Trend.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import style from "./Trend.module.css";
-import { Hashtag } from "@/model/HashTag";
+import { Hashtag } from "@/model/Hashtag";
 
 type Props = {trend: Hashtag}
 
 export default function Trend({trend}:Props) {
   return (
-    <Link href={`/search?q=${trend.title}`} className={style.container}>
+    <Link href={`/search?q=${encodeURIComponent(trend.title)}`} className={style.container}>
       <div className={style.count}>실시간트렌드</div>
       <div className={style.title}>{trend.title}</div>
       <div className={style.count}>{trend.count.toLocaleString()} posts</div>
